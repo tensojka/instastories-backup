2f7962ea1791a5c922fe97478cea973eef405dde
Ondrej Sojka <hoschiCZ@users.noreply.github.com>
2017-04-13 13:02:59 +0200

    Add support for downloading media from IG,  move auth to separate file.
    File backupfiles.py crunches thru all entries in stories.sqlite3 and removes ones that are 404'd, saves others into files/.
    
    Going to consolidate these two files into one in near future.
    
    I've also moved sensitive credentials to a separate, gitignored file to prepare this for open-sourcing. The file is named prefs.json, I will make the an example available once this goes open source.

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
+#!/usr/bin/python3
 import requests
 import sqlite3
+import json
 
 conn = sqlite3.connect('stories.sqlite3')
+c = conn.cursor()
 
-ids_tobackup = {
-    '325362233': 'cestujurada',
-    '655295824': 'hoschicz',
-    '4197576288': 'malejbuk',
-    '270679163': 'nejskvelejsi',
-    '2031881575': 'jakub.sustr2',
-    '503530161': 'chcukiwi',
-    '551854863': 'chceskiwi',
-    '392366908': 'petahrdina_',
-    '1231421545': 'sara_smejkalova',
-    '3250038853': 'tobiaskosir',
-    '1693587948': 'vopelkova.desi',
-    '1470027527': 'nikcakikca007',
-    '1245472971': 'filipvalicek'
-}
+with open('prefs.json') as json_data:
+    prefs = json.load(json_data)
+ids = prefs["ids"]
+headers = prefs["headers"]
 
-while len(ids_tobackup) > 0:
-    pair = ids_tobackup.popitem()
-    headers = {
-        'cookie': "ds_user_id=655295824; sessionid=IGSC4d666e5ff7db9db3e4262f1fd75c20fb0379f358711eaa86603b7af68ce5c0ea%3AW2miaupIRUAT1m3OBlAC1xs5hniIFhGL%3A%7B%22asns%22%3A%7B%22time%22%3A1489478310%2C%2278.102.117.227%22%3A6830%7D%2C%22last_refreshed%22%3A1489478310.2239192%2C%22_token_ver%22%3A2%2C%22_platform%22%3A4%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_token%22%3A%22655295824%3Azi6WHai8n0mRXjj3dYonEslwAXxXYOh8%3A170836c056f67eb80949b6bbd4e4cd48a96fee6d947108d0f23224d8045c95b4%22%2C%22_auth_user_id%22%3A655295824%2C%22_auth_user_hash%22%3A%22%22%7D",
-        'user-agent': "Instagram 10.3.2 (iPhone7,2; iPhone OS 9_3_3; en_US; en-US; scale=2.00; 750x1334) AppleWebKit/420+",
-        'cache-control': "no-cache"
-        }
-    response = requests.request("GET", "https://i.instagram.com/api/v1/feed/user/655295824/reel_media/", headers=headers)
-    print(response.text)
+while len(ids) > 0:
+    pair = ids.popitem()
+    response = requests.request("GET", "https://i.instagram.com/api/v1/feed/user/"+pair[0]+"/reel_media/", headers=headers)
+    response = json.loads(response.text)
+    for item in response['items']:
+        id = item['id']
+        try:
+            url = item['video_versions'][0]['url']
+        except KeyError: #if there are no videos of this item
+            url = item['image_versions2']['candidates'][0]['url']
+        userid = pair[0]
+        username = pair[1]
+        taken_at = item['taken_at']
+        try:
+            c.execute('INSERT INTO entries VALUES(?,?,?,?,?,?)', (str(id),str(url),str(userid),str(username),taken_at,""))
+        except sqlite3.IntegrityError: #is thrown when UNIQUE doesn't match
+            print('!', end='',flush=True)
+        else:
+            print('.', end='',flush=True)
+    
+conn.commit()
     #CREATE TABLE `entries` (
     #    `id`	TEXT UNIQUE,
     #    `url`	TEXT,
