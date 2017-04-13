d70ae541dc6b2bc9cf18f041f0353ac903799956
Ondrej Sojka <hoschiCZ@users.noreply.github.com>
2017-04-13 13:20:25 +0200

    Consolidate backupfiles.py and main.py, clarify CLI output

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import requests
 import sqlite3
 import json
 
-conn = sqlite3.connect('stories.sqlite3')
-c = conn.cursor()
-
-with open('prefs.json') as json_data:
-    prefs = json.load(json_data)
+try:
+    with open('prefs.json') as json_data:
+        prefs = json.load(json_data)
+except FileNotFoundError:
+    print("prefs.json file not found in current directory. Exiting.")
+    exit()
 ids = prefs["ids"]
 headers = prefs["headers"]
+
+conn = sqlite3.connect(prefs["dbfilename"])
+c = conn.cursor()
+
+print("Loading new entries to DB")
 
 while len(ids) > 0:
     pair = ids.popitem()
@@ -30,7 +36,37 @@ while len(ids) > 0:
             print('!', end='',flush=True)
         else:
             print('.', end='',flush=True)
-    
+
+conn.commit()
+
+print("Downloading new videos and photos, printing names of new files:")
+
+todelete = []
+toupdate = []
+
+for row in c.execute('SELECT * FROM entries WHERE filename = ""'):
+    r = requests.get(row[1])
+    if r.status_code == 404:
+        todelete.append(row[0])
+    elif r.status_code%200 < 100:
+        if r.headers["Content-Type"] == "video/mp4":
+            filename = str(row[3])+":"+str(row[4])+".mp4"
+        elif r.headers["Content-Type"] == "image/jpeg":
+            filename = str(row[3])+":"+str(row[4])+".jpg"
+        else:
+            filename = str(row[3])+":"+str(row[4])+".unknown"
+            print("WARNING: couldn't identify MIME type for URL "+row[1])
+        with open(prefs["filesdir"]+"/"+filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+        toupdate.append((filename,row[0]))
+        print(filename)
+
+for item in todelete:
+    c.execute('DELETE FROM entries WHERE id = ?',(item,))
+for item in toupdate:
+    c.execute('UPDATE entries SET filename = ? WHERE id = ?',item)
 conn.commit()
     #CREATE TABLE `entries` (
     #    `id`	TEXT UNIQUE,
